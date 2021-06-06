0c529cd0d3f64b07ce4da2b3a9dacd0595791a83
Zuschlag <zuschlag@pop-os.localdomain>
2021-06-06 11:11:10 -0700

    Replace line counter with run-length encoding

--- src/vm/chunk.rs
+++ src/vm/chunk.rs
@@ -19,7 +19,7 @@ impl Chunk {
 
     pub fn write(&mut self, byte: OpCode, line: usize) {
         self.code.push(byte);
-        self.lines.push(line);
+        self.write_line(line);
     }
 
     pub fn add_constant(&mut self, value: Value) -> usize {
@@ -35,13 +35,32 @@ impl Chunk {
         println!("== {} ==", name);
         for (offset, byte) in self.code.iter().enumerate() {
             print!("{:04} ", offset);
-            if offset > 0 && self.lines[offset] == self.lines[offset - 1] {
+            if offset > 0 && self.get_line(offset) == self.get_line(offset - 1) {
                 print!("    | ");
             } else {
-                print!("{number:>width$} ", number = self.lines[offset], width = 5);
+                print!("{number:>width$} ", number = self.get_line(offset), width = 5);
             }
             self.disassamble_instruction(byte);
         }
+    }
+
+    fn write_line(&mut self, line: usize) {
+        let current_line = self.lines.len();
+        if current_line == line {
+            self.lines[current_line - 1] += 1;
+        } else {
+            self.lines.push(1);
+        }
+    }
+
+    fn get_line(&self, offset: usize) -> usize {
+        let mut current_index = 0;
+        let mut line_counter = self.lines[current_index];
+        while line_counter <= offset && current_index < self.lines.len() {
+            line_counter += self.lines[line_counter];
+            current_index += 1;
+        }
+        current_index + 1
     }
 
     fn disassamble_instruction(&self, instruction: &OpCode) {
